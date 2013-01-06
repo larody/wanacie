f24491b7d49a5beddf560f5d9fa9ce083f30d52c
larody <throwbases@gmail.com>
2013-01-06 22:35:12 +0900

    2013-01-06 22:35:12 +0900

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
   def result
     @events = Event.where('place LIKE ? AND purpose LIKE ?',
                 "%#{params[:place]}%", "%#{params[:purpose]}%").
-                paginate(:page => params[:page], :per_page => 1)
+                upcoming(10).paginate(:page => params[:page], :per_page => 10)
   end
 
 end
